b29330d01fe1a659b3b003c039192626489969c8
Mrnux <mrnux@nux.com>
2019-07-30 21:51:32 +0900

    Remove a wrongly added const descriptor

--- src/libopencl.c
+++ src/libopencl.c
@@ -350,7 +350,7 @@ clCreateCommandQueue(cl_context                     context,
 cl_command_queue
 clCreateCommandQueueWithProperties(cl_context                     context,
                      cl_device_id                   device,
-                     const cl_queue_properties *properties,
+                     cl_queue_properties *properties,
                      cl_int *                       errcode_ret)
 {
     f_clCreateCommandQueueWithProperties func;
