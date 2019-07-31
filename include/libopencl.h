2c095cd0fc4c5dd24d9d5960f71a9a703c8181c2
Mrnux <mrnux@nux.com>
2019-07-31 21:07:12 +0900

    Change withProperties definition to match the one in the real opencl header

--- include/libopencl.h
+++ include/libopencl.h
@@ -39,7 +39,7 @@ typedef cl_int (*f_clGetContextInfo) (cl_context, cl_context_info, size_t, void 
 
 typedef cl_command_queue (*f_clCreateCommandQueue) (cl_context, cl_device_id, cl_command_queue_properties, cl_int *);
 
-typedef cl_command_queue (*f_clCreateCommandQueueWithProperties) (cl_context, cl_device_id, cl_command_queue_properties *, cl_int *);
+typedef cl_command_queue (*f_clCreateCommandQueueWithProperties) (cl_context, cl_device_id, const cl_command_queue_properties *, cl_int *);
 
 typedef cl_int (*f_clRetainCommandQueue) (cl_command_queue);
 
